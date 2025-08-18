5c729eb87a92fd990fab97892984f11575c8a273
mahdi rezaii <mahdirezaii490@gmail.com>
2025-08-18 15:09:36 +0330

    eslint rule disabled t3 standalone added configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  // Add the packages in transpilePackages
+  transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+
 };
 
 export default nextConfig;
