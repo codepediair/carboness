d4843eaba182e61b93fa128ec95766f021251b93
mahdi rezaii <mahdirezaii490@gmail.com>
2025-08-18 16:47:03 +0330

    eslint disable rules added

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  // Add the packages in transpilePackages
-  transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
+    eslint: {
     ignoreDuringBuilds: true,
   },
-
 };
 
 export default nextConfig;
