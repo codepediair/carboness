aab9882e768bb4c004ebcc099f505a9b43386cf0
mahdi rezaii <mahdirezaii490@gmail.com>
2025-08-23 17:22:22 +0330

    create admin page layout and functionality it

---
+++ app/admin/carbons/page.tsx
@@ -0,0 +1,18 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function CarbonsPage() {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Your Carbons</h1>
+
+        <Link className={buttonVariants()} href="/admin/carbons/create">Create Carbon</Link>
+      </div>
+
+      <div>
+        <p>Here you will see all of the carbons</p>
+      </div>
+    </>
+  )
+}
