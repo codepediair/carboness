d773758f73922e69b46b5fc412d04bdd99cf67ec
mahdi rezaii <mahdirezaii490@gmail.com>
2025-11-18 04:36:49 +0330

    user input form added

--- app/admin/carbons/create/page.tsx
+++ app/admin/carbons/create/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { db } from "@/db/drizzle";
+import { UserInputForm } from "./_components/userInputForm";
 
 export default async function CarbonCreatePage() {
   const session = await auth.api.getSession({
@@ -10,5 +11,9 @@ export default async function CarbonCreatePage() {
   if (session === null) {
     return redirect("/login");
   }
-  return <div>Create Carbon</div>;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <UserInputForm currentUserId={session.user.id} />
+    </div>
+  );
 }
